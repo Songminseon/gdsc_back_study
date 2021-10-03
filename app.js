dedea1e85768590c22c0dbd320f95f50f15b100c
Songminseon <smsun0329@naver.com>
2021-10-04 01:01:07 +0900

    cors 추가

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 const path = require("path");
 const session = require("express-session");
